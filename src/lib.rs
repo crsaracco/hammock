e1249588c2be76592c88b8bd634058b7b4f15ec6
Charles Saracco <crsaracco@gmail.com>
2019-02-22 20:43:14 -0500

    skeleton

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub struct Hammock {
+
+}
+
+impl Hammock {
+    pub fn new() -> Self {
+        Self {
+            
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
