39d580ec148eed17b8ac7ece0abb82d248630161
Charles Saracco <crsaracco@gmail.com>
2019-02-25 18:03:27 -0500

    Pretty basic API client

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # hammock
+
 A crate to help you interact with REST APIs.
+
+APIs usually require you to set a custom user agent and to rate limit your requests,
+which I found to be pretty boilerplate in the various API-consuming programs I kept on writing.
+So, I figured I'd abstract all that boilerplate into this crate.
+
+## Using
+
+```rust
+// Set the ratelimiting settings.
+// In this example, you're allowing Hammock to fetch an API endpoint once per second.
+// Hammock requests will block if you make requests faster than this, until it's been one second
+// since the last request.
+let duration = Duration::new(1, 0);
+
+// Set the user agent that Hammock will use when making API requests.
+let user_agent = "MyApiConsumer v0.1.1 (user@example.com)";
+
+// Actually create the Hammock object.
+let mut hammock = Hammock::new(duration, user_agent).unwrap();
+
+// Make a GET request, get the response body back as plaintext.
+println!("{}", hammock.get_text("http://some.website/api/get_users")?);
+
+// Make a GET request; get the response body back as a Serde JSON object.
+println!("{:#?}", hammock.get_json("http://some.website/api/get_users?fmt=json")?);
+
+```
+
+## TODO
+
+ - More customizable rate limiting
+ - Authentication
+ - Support for `POST`, `PUT`, etc.
+
+## License
+
+MIT. See the `LICENSE` file for more information.
+
+Any contributions to this repository are assumed to be MIT unless specified otherwise.
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,53 @@
-pub struct Hammock {
+extern crate ratelimit;
+extern crate reqwest;
+extern crate serde_json;
 
+use std::time::Duration;
+use ratelimit::Limiter;
+
+pub struct Hammock {
+    reqwest_client: reqwest::Client,
+    ratelimit: Limiter,
 }
 
 impl Hammock {
-    pub fn new() -> Self {
-        Self {
-            
-        }
-    }
-}
+    /// Create a Hammock that rate-limits requests to one request per `duration`.
+    pub fn new(duration: Duration, user_agent: &str) -> Result<Self, Box<std::error::Error>> {
+        let ratelimit = ratelimit::Builder::new()
+            .capacity(1) //number of tokens the bucket will hold
+            .quantum(1) //add one token per interval
+            .interval(duration) //add quantum tokens every `duration`
+            .build();
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut reqwest_headers = reqwest::header::HeaderMap::new();
+        reqwest_headers.insert(
+            reqwest::header::USER_AGENT,
+            reqwest::header::HeaderValue::from_str(user_agent)?
+        );
+
+        let reqwest_client = reqwest::Client::builder()
+            .default_headers(reqwest_headers)
+            .build()?;
+
+        Ok(Self {
+            reqwest_client,
+            ratelimit,
+        })
+    }
+
+    pub fn get_text(&mut self, url: &str) -> Result<String, Box<std::error::Error>> {
+        self.ratelimit.wait();
+        let response_body = self.reqwest_client.get(url)
+            .send()?
+            .text()?;
+        Ok(response_body)
+    }
+
+    pub fn get_json(&mut self, url: &str) -> Result<serde_json::Value, Box<std::error::Error>> {
+        self.ratelimit.wait();
+        let json: serde_json::Value = self.reqwest_client.get(url)
+            .send()?
+            .json()?;
+        Ok(json)
     }
 }
